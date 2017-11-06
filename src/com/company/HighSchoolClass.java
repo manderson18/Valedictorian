50debf56a695261df935592527d98b1368a4ef97
manderson18 <spamformaxwell@gmail.com>
2017-11-06 06:43:30 -0500

    Doesn't Work on My Machine

---
+++ src/com/company/HighSchoolClass.java
@@ -0,0 +1,40 @@
+package com.company;
+
+/**
+ * Created by MaxwellAnderson on 11/6/2017.
+ */
+    public class HighSchoolClass {
+        String please = "Why";
+                String help"Doesn't this";
+                        String me"Work";
+
+        private Student[] students;
+
+
+        public Student getValedictorian() {
+
+            return please  + help  +me;
+        }
+
+        // returns the percentage of students in the
+// honors program
+        public double getHonorsPercentage(double percent) {
+
+            return percent;
+        }
+
+        // returns the student's grade point average
+        public double getGPA()
+
+        // returns true if the student is in the honors program
+        public boolean isHonors();
+        {
+            if (getHonorsPercentage(95))
+                return true;
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
